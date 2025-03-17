42848c1fde6ce2eec6498ddf0039abf84403ac48
gschacon <chacon.gsc@gmail.com>
2025-03-17 05:51:49 -0300

    formated main file

--- main.py
+++ main.py
@@ -10,23 +10,24 @@ load_dotenv()
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
+
 def main(path_to_pdf):
     db_exists, path_to_db, chunks = PdfHandler(path_to_pdf).pdf_handler()
 
     if db_exists:
-        print('File already exists in database. \nLoading vector store...')
+        print("File already exists in database. \nLoading vector store...")
         vectore_store = VectorStore(db_exists, path_to_db, chunks)
         faiss_index = vectore_store.handle_vector_store()
         print("Vector store loaded successfully!")
     else:
-        print('File does not exist in database. \nCreating vector store...')
+        print("File does not exist in database. \nCreating vector store...")
         vectore_store = VectorStore(db_exists, path_to_db, chunks)
         faiss_index = vectore_store.handle_vector_store()
         print("Vector store created successfully!")
 
-    user_input = HandleUserInput().ask_user_input(first_message = True)
+    user_input = HandleUserInput().ask_user_input(first_message=True)
     if HandleUserInput().check_end(user_input):
-        print('Thank you for using chat-with-paper!')
+        print("Thank you for using chat-with-paper!")
         return
     docs = vectore_store.similarity_search(faiss_index, user_input)
     llm = ChatLLMOpenAi(OPENAI_API_KEY)
@@ -34,14 +35,17 @@ def main(path_to_pdf):
     print(answer)
 
     while True:
-        new_user_input = HandleUserInput().ask_user_input(first_message = False)
+        new_user_input = HandleUserInput().ask_user_input(first_message=False)
         if HandleUserInput().check_end(new_user_input):
             break
         docs = vectore_store.similarity_search(faiss_index, new_user_input)
-        answer, chat_prompt_message = llm.continue_chat(answer, new_user_input, docs, chat_prompt_message)
+        answer, chat_prompt_message = llm.continue_chat(
+            answer, new_user_input, docs, chat_prompt_message
+        )
         print(answer)
-    print('Thank you for using chat-with-paper!')
+    print("Thank you for using chat-with-paper!")
     return
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
